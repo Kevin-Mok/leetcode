d0ae7ebeb8904bb5d885ef158d3be56d2c10e77d
Kevin Mok <kevin.mok@mail.utoronto.ca>
2022-06-10 21:06:45 -0400

    894 solution

--- dynamic-programming/894-full-binary-trees.py
+++ dynamic-programming/894-full-binary-trees.py
@@ -13,35 +13,70 @@ class TreeNode:
         return f"{self.val} [{self.left}] [{self.right}]"
 
 class Solution:
+    #  def allPossibleFBT(self, n: int) -> list[[TreeNode]]:
+        #  #  possible_trees = [[] * n]
+        #  possible_trees = []
+        #  for i in range(n + 1):
+            #  possible_trees.append([])
+        #  print(possible_trees)
+        #  for i in range(n + 1):
+            #  if i % 2 == 0:
+                #  continue
+            #  elif i == 1:
+                #  possible_trees[i].append(TreeNode(0))
+            #  elif i == 3:
+                #  possible_trees[i].append(TreeNode(0, 0, 0))
+            #  else:
+                #  #  print(i)
+                #  #  for left_tree in possible_trees[i - 2]:
+                    #  #  for right_tree in possible_trees[i - 4]:
+                        #  #  print(left_tree, right_tree)
+                        #  #  #  print(left_tree)
+                        #  #  #  print(right_tree)
+                        #  #  possible_trees[i].append(TreeNode(0,
+                            #  #  left_tree, right_tree))
+                #  #  print()
+                #  #  for left_tree in possible_trees[i - 4]:
+                    #  #  for right_tree in possible_trees[i - 2]:
+                        #  #  print(left_tree, right_tree)
+                        #  #  possible_trees[i].append(TreeNode(0,
+                            #  #  left_tree, right_tree))
+                #  #  print()
+        #  return possible_trees[n]
+
     def allPossibleFBT(self, n: int) -> list[[TreeNode]]:
         #  possible_trees = [[] * n]
         possible_trees = []
         for i in range(n + 1):
             possible_trees.append([])
-        print(possible_trees)
+        #  print(possible_trees)
         for i in range(n + 1):
             if i % 2 == 0:
                 continue
             elif i == 1:
                 possible_trees[i].append(TreeNode(0))
-            elif i == 3:
-                possible_trees[i].append(TreeNode(0, 0, 0))
             else:
-                print(i)
-                for left_tree in possible_trees[i - 2]:
-                    for right_tree in possible_trees[i - 4]:
-                        print(left_tree, right_tree)
-                        #  print(left_tree)
-                        #  print(right_tree)
-                        possible_trees[i].append(TreeNode(0,
-                            left_tree, right_tree))
-                print()
-                for left_tree in possible_trees[i - 4]:
-                    for right_tree in possible_trees[i - 2]:
-                        print(left_tree, right_tree)
-                        possible_trees[i].append(TreeNode(0,
-                            left_tree, right_tree))
-                print()
+                left_counter = 1
+                right_counter = i - 2
+                while left_counter <= right_counter:
+                    #  print(i, left_counter, right_counter)
+                    for left_tree in possible_trees[left_counter]:
+                        for right_tree in possible_trees[right_counter]:
+                            #  print(left_tree, right_tree)
+                            #  print(left_tree)
+                            #  print(right_tree)
+                            possible_trees[i].append(TreeNode(0,
+                                left_tree, right_tree))
+                    #  print()
+                    if left_counter != right_counter:
+                        for left_tree in possible_trees[right_counter]:
+                            for right_tree in possible_trees[left_counter]:
+                                #  print(left_tree, right_tree)
+                                possible_trees[i].append(TreeNode(0,
+                                    left_tree, right_tree))
+                    #  print()
+                    left_counter += 2
+                    right_counter -= 2
         return possible_trees[n]
 
 if __name__ == "__main__":
@@ -49,3 +84,4 @@ if __name__ == "__main__":
     #  print(sol.allPossibleFBT(7))
     print(len(sol.allPossibleFBT(7)))
     #  print(len(sol.allPossibleFBT(5)))
+    #  print(len(sol.allPossibleFBT(3)))
